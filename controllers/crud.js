273ce2b898f22d1fd3c12c2481a0dce86cf6023c
camilo <camilo.diaz.dev@gmail.com>
2022-09-11 23:07:07 -0300

    Se creo funcion buscar

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,5 +1,7 @@
 const conexion = require('../database/db');
 
+let productos;
+
 function listarProductos(req, res){
     conexion.query('SELECT id, name, url_image, price, discount, category FROM product', (error, results)=>{
         if(error){
@@ -16,6 +18,7 @@ function listarProductos(req, res){
             );
             return;
         }else{
+            productos = results;
             res.send(results);
         }
     })
@@ -42,27 +45,40 @@ function listarProductosPorId(req, res){
     })
 }
 
-// function listarProductosPorNombre(req, res){
-//     console.log(req.params.name)
-//     conexion.query(`SELECT id, name, url_image, price, discount, category FROM product WHERE name = "${req.params.name}"`, (error, results)=>{
-//         if(error){
-//             res.send(error);
-//             // throw error;
-//         }else if(results.length === 0){
-//             res.send(
-//                 {
-//                     data: "ok",
-//                     resultado: results.length
-//                 }
-//             );
-//             return;
-//         }else{
-//             res.send(results);
-//         }
-//     })
-// }
+function buscarProducto(req, res){
+
+    if(!req.params.palabra){
+        res.send(
+            {
+                data: "ok",
+                resultado: productos
+            }
+        );
+        return
+    }
+
+    const newArray = []
+
+    for (const producto of productos) {
+        let nombre = producto.name.toLowerCase();
+        if( nombre.indexOf(req.params.palabra.toLowerCase()) !== -1){
+            newArray.push(producto);
+        }
+    }
+
+    res.send(
+        {
+            data: "ok",
+            resultado: newArray
+        }
+    );
+
+
+}
+
 
 module.exports = {
     listarProductos         : listarProductos,
-    listarProductosPorId    : listarProductosPorId
+    listarProductosPorId    : listarProductosPorId,
+    buscarProducto          : buscarProducto
 };
--- router.js
+++ router.js
@@ -5,6 +5,7 @@ const crud = require("./controllers/crud");
 router.
     get("/", crud.listarProductos)
     .get("/:id", crud.listarProductosPorId)
+    .get("/buscar/texto/:palabra", crud.buscarProducto)
     // .get("/name/:name", crud.listarProductosPorNombre)
 
 
