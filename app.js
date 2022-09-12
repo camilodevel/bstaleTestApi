0a987d5f949dafe35cb926f07dda36cb6fdd2a6f
camilo <camilo.diaz.dev@gmail.com>
2022-09-12 17:44:44 -0300

    cambios de heroku

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const router = require('./router');
 const cors = require('cors');
-
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 
 app.use("/bsaletest/v1/products/", router);
 
-app.listen(5000, () => {
-    console.log('Serrver corriendo en http://localhost:5000');
+app.listen(PORT, () => {
+    console.log('Serrver corriendo en http://localhost:'+PORT);
 });
